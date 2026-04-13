55e5d327b8afb3afd542d18f88785f20c3d4ff54
WEBDEVELOPER-99 <gatishsahoo@gmail.com>
2026-04-13 21:37:23 +0530

    changes

--- domIIandobjectprototypes.js
+++ domIIandobjectprototypes.js
@@ -1,38 +1,45 @@
-// * methods of selections - 
-
-// 1. getElementsbyId
-// 2. getElementsbyClassName
-// 3. getElementsbyTagName
+// * METHODS OF SELECTIONS - 
+// 1. getElementById
+// 2. getElementsByClassName
+// 3. getElementsByTagName
 // 4. querySelector
 // 5. querySelectorAll
+
 
 
 async function handleApi() {
     try {
         const response = await fetch("https://jsonplaceholder.typicode.com/users");
         const data = await response.json();
-       const table = document.getElementById("collection");
-       data.map((item) => {
-        table.innerHTML += `<tbody>
-         <tr>
-        <td>${item.id}</td>
-        <td>${item.name}</td>
-        <td>${item.username}</td>
-        <td>${item.email}</td>
-        </tr>
-        </tbody>`;
-         });
+        console.log(data);
+
+        const table = document.getElementById("collection");
+
+        data.map((item, index) => {
+            table.innerHTML += `
+            <tr>
+                <td>${item.id}</td>
+                <td>${item.name}</td>
+                <td>${item.username}</td>
+                <td>${item.email}</td>
+                <td><button onclick="deleteRow(this)">Delete</button></td>
+            </tr>`;
+        });
+
     } catch (error) {
         console.log(error);
     }
 }
+
 handleApi();
 
-const password = prompt("Enter password to view:");
-if (password ==="JK FAN") {
-  
-} else {
-  document.body.innerHTML = "<h1 style='color: red; text-align: center; margin-top: 50px;'>Access Denied</h1><p style='color: red; text-align: center;'>The password you've entered is incorrect. Please try again</p>";
+function deleteRow(button) {
+    const row = button.parentElement.parentElement;
+    row.remove();
 }
 
+const password = prompt("Enter password to view:"); if (password ==="JK FAN") { 
 
+} else 
+{ document.body.innerHTML = 
+    "<h1 style='color: red; text-align: center; margin-top: 50px;'>Access Denied</h1><p style='color: red; text-align: center;'>The password you've entered is incorrect. Please try again</p>"; }
